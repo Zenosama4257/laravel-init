400962f7dfdfde22f8ec6c9d09b8208e49084830
Juan <jjalabertriquelme@hotmail.com>
2024-02-20 19:39:05 +0100

    version1.0

--- resources/views/_partials/macros.blade.php
+++ resources/views/_partials/macros.blade.php
@@ -1,1 +1,1 @@
-<img class="avatar" src="https://yt3.ggpht.com/u2s-JIvKEHT8x90FWwoOel6-FS5jnics_4p7BQflEASY-xaCh1YWq_0DsrB4RyWmevm7dMv7ag=s900-c-k-c0x00ffffff-no-rj" srcset="https://yt3.ggpht.com/u2s-JIvKEHT8x90FWwoOel6-FS5jnics_4p7BQflEASY-xaCh1YWq_0DsrB4RyWmevm7dMv7ag=s900-c-k-c0x00ffffff-no-rj" alt="El Rincón de Isma Avatar">
+<img class="avatar" src="https://media.licdn.com/dms/image/C4D0BAQGyUUQ2OSepUQ/company-logo_200_200/0/1630472852168/zataca_systems_s_l_logo?e=2147483647&v=beta&t=mb9zxQXIwbqynXumF9ZRxxWwQaTwmpG1SQbLrYGIBVM" alt="Zataca Logo">
